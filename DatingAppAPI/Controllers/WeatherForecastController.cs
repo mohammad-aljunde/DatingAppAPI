5f1e6bd11f38966ec3e19ce073e7adbe001df5fb
Mohammad Aljundi <M.Aljundi@sirocomena.com>
2021-07-22 01:20:39 +0300

    Changeing the list

--- DatingAppAPI/Controllers/WeatherForecastController.cs
+++ DatingAppAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DatingAppAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", " Mohamamd", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
